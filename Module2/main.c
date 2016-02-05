e18b44bf4663675b52699b21eb7a787333d2ef75
Mohannad Abwah <2223788+mohannad-abwah@users.noreply.github.com>
2016-02-05 00:20:29 -0800

    HandmadeQuake 2.3 - Added basic time-keeping in game loop

--- Module2/main.c
+++ Module2/main.c
@@ -1,13 +1,16 @@
 #include <Windows.h>
+#include <stdio.h>
 
 #define WINDOW_CLASS_NAME L"Module2"
-#define WINDOW_NAME L"Lesson 2.1"
+#define WINDOW_NAME L"Lesson 2.3"
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
 BOOL isRunning = TRUE;
 
 LRESULT CALLBACK mainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+void dprintf(char* format, ...);
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -38,14 +41,19 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		NULL,
 		hInstance,
 		0);
-	
+
 	ShowWindow(mainWindow, SW_SHOWDEFAULT);
 
 	HDC deviceContext = GetDC(mainWindow);
 	PatBlt(deviceContext, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, BLACKNESS);
 	ReleaseDC(mainWindow, deviceContext);
 
+	const LARGE_INTEGER frequency;
+	LARGE_INTEGER tick, tock;
 	MSG msg;
+
+	QueryPerformanceFrequency(&frequency);
+	QueryPerformanceCounter(&tick);
 	while (isRunning)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
@@ -53,6 +61,17 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+
+		QueryPerformanceCounter(&tock);
+
+		LONGLONG interval = tock.QuadPart - tick.QuadPart;
+		double timeLapsed = (double) interval / frequency.QuadPart;
+
+		dprintf("Time elapsed: %3.7f\n", timeLapsed);
+
+		QueryPerformanceCounter(&tick);
+
+		Sleep(100);
 	}
 
 	return EXIT_SUCCESS;
@@ -73,4 +92,19 @@ LRESULT CALLBACK mainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 
 	return result;
+}
+
+void dprintf(char* format, ...)
+{
+	va_list argptr;
+	va_start(argptr, format);
+
+	size_t n = vsnprintf(NULL, 0, format, argptr) + sizeof(char);
+	char* buffer = (char*) malloc(n * sizeof(char));
+
+	vsnprintf(buffer, n, format, argptr);
+	OutputDebugStringA(buffer);
+
+	va_end(argptr);
+	free(buffer);
 }
